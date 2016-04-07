ffc816725c55b80f435319b5587bc5fbc7137edb
Abram Flansburg <abe.flansburg@gmail.com>
2016-04-07 11:51:47 -0500

    Used comprehension to rewrite 6-part mac segments to include ":" to match wireless controller .log file.

--- netmac-find.py
+++ netmac-find.py
@@ -13,7 +13,12 @@ with open('MAC.CSV', 'rb') as mac_csv:
     macreader = csv.reader(mac_csv, delimiter='\n')
     for row in macreader:
         for i in row:
+            i = ':'.join(i[e:e+2] for e in range(0,6,2))
             maclist.append(i)
 
+with open('WING.log', 'rb') as logfile:
+    for mac_segment in maclist:
+        if mac_segment in logfile.read():
+            print "This macsegment, {0}, was found".format(mac_segment)
 
-
+logfile.close()
