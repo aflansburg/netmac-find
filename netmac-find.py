ad59324c41b0b41cb27237a642fa5106fc213527
Abram Flansburg <abe.flansburg@gmail.com>
2016-04-07 13:15:16 -0500

    Using mmap to create an object using the log file to read in stead of reading the file at each iteration of the if statement.

--- netmac-find.py
+++ netmac-find.py
@@ -3,7 +3,7 @@ __author__ = 'misaflansb'
 # Will use tailored .csv for this one
 
 import csv
-
+import mmap
 
 # read MAC addresses from .csv file into iterable list
 
@@ -13,12 +13,15 @@ with open('MAC.CSV', 'rb') as mac_csv:
     macreader = csv.reader(mac_csv, delimiter='\n')
     for row in macreader:
         for i in row:
-            i = ':'.join(i[e:e+2] for e in range(0,6,2))
+            i = '-'.join(i[e:e+2] for e in range(0,6,2))
             maclist.append(i)
 
-with open('WING.log', 'rb') as logfile:
-    for mac_segment in maclist:
-        if mac_segment in logfile.read():
-            print "This macsegment, {0}, was found".format(mac_segment)
+confirmed_mac = []
 
-logfile.close()
+read_file = open('WING.log')
+s = mmap.mmap(read_file.fileno(), 0, access=mmap.ACCESS_READ)
+
+for mac_segment in maclist:
+    if s.find(mac_segment) != -1:
+            print "This macsegment, {0}, was found".format(mac_segment)
+            confirmed_mac.append(mac_segment)
